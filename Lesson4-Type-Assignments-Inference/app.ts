8f10b0990f51cf254c69fa43c680ace8b9510fa3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-17 13:08:32 +0300

    practsied type inference

---
+++ Lesson4-Type-Assignments-Inference/app.ts
@@ -0,0 +1,10 @@
+//ayrı şekillerde de atamaa yapılabilir
+let number1 : number;
+number1 = 10;
+
+
+//ilk başta string atandığı için türü otomatik olarak string olarak atanır 
+let phrase = 'str';
+phrase = 1;
+
+
