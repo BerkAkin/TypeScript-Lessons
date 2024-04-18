cb880ba551ac0c0c4a3fd8e7e78e1be685de2cd1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-18 20:13:52 +0300

    practised custom, void, undefined return types and function type and custom function type with arrow function notation

--- Lesson10-FunctionReturnTypes/app.ts
+++ Lesson10-FunctionReturnTypes/app.ts
@@ -19,5 +19,23 @@ function printUn(str:string): undefined{
     return;
 }
 
-
 printResult(who('Berk').name);
+
+
+//Fonksiyona fonksiyon atama işlemi. Currying parçasıdır aslında. Bu çalışır ama tür belirtmeden araya yorum satırındaki kod eklenirse bozulabilir
+//bunun için de Function türünden yararlanılır;
+function com(n1:number,n2:number){
+    return +n1 + +n2;
+}
+let combineValues = com;
+// combineValues = 5;
+combineValues(5,5);
+
+//FONKSİYON TÜRÜ
+let combine: Function
+combine = 6; //hata verir çünkü 6 fonksiyon değildir
+combine = printResult; //çalışır çünkü atanan şey fonksiyondur
+
+
+//combine2 türü herhangi bir parametresi olmayan ve dönüş değeri number olan herhangi bir fonksiyonu kabul edebilir demek oluyor.
+let combine2: () => number;
