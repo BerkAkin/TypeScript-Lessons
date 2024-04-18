47d26d4f6393a1848e5bfeb2f972b150fa5d8914
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-18 16:05:09 +0300

    minor change

--- Lesson7-Enum-Any-Union-Types/app.ts
+++ Lesson7-Enum-Any-Union-Types/app.ts
@@ -1,5 +1,5 @@
+//ENUM
 enum Role { ADMIN, READ_ONLY, AUTHOR };
-
 enum Role2 { ADMIN = 5, AUTHOR="AUTHOR"}
 
 const person = {
@@ -26,5 +26,5 @@ let obje :{
     name:"Berk",
     age: 24,
     hobbies:["camping","fishing"],
-    role: Role.AUTHOR
+    role: Role2.AUTHOR
 };
