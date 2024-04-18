11e0b3c9f4489ce7018857ed6030d35f13808a00
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-18 22:20:42 +0300

    practised never type

--- Lesson11-Callbacks-UnknownNeverTypes/app.ts
+++ Lesson11-Callbacks-UnknownNeverTypes/app.ts
@@ -1,3 +1,4 @@
+//callbacks
 function addAndHandle(n1:number, n2:number, cb: (num: number) => void){
     const result = n1 + n2;
     cb(result)
@@ -6,3 +7,28 @@ function addAndHandle(n1:number, n2:number, cb: (num: number) => void){
 addAndHandle(10, 20, (result)=>{
     console.log(result);
 });
+
+
+
+
+//unknown tipi
+let userInput: unknown;
+let userName: string;
+userInput = 5;
+userInput = "Max";
+
+userName = userInput //hata verir çünkü inputun tip unknowndur. Any olsa hata vermez ama unknown daha kararlıdır. bunun yerine kontrol edilerek yapılabilir 
+if(typeof userInput === "string"){
+    userName = userInput
+}
+
+
+
+
+
+//never type
+function generateError(message:string,code:number): never {
+    throw {message:message, errorCode:code};
+}
+
+generateError('An error has occured', 500)
