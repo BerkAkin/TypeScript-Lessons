9af412d25df2caf881624a83943cf441077026cc
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-25 01:10:32 +0300

    practsied private and public access modifiers

--- Lesson22-Classes/app.js
+++ Lesson22-Classes/app.js
@@ -1,14 +1,26 @@
 "use strict";
 class Department {
-    constructor(n) {
-        this.name = n;
-    }
-    describe() {
-        console.log("Department: " + this.name);
-    }
+  constructor(n) {
+    this._employees = [];
+    this.name = n;
+  }
+  describe() {
+    console.log("Department: " + this.name);
+  }
+  addEmployee(employee) {
+    this._employees.push(employee);
+  }
+  printEmployeeInformation() {
+    console.log(this._employees.length);
+    console.log(this._employees);
+  }
 }
 let accounting = new Department("Accounting");
 console.log(accounting);
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
 accounting.describe();
-const accountingCopy = { name: "dummy", describe: accounting.describe };
-accountingCopy.describe();
+//nesne kopyalama işlemi
+//const accountingCopy = { name: "dummy", describe: accounting.describe };
+//accountingCopy.describe();
--- Lesson22-Classes/app.ts
+++ Lesson22-Classes/app.ts
@@ -1,16 +1,31 @@
 class Department {
   name: string;
+  private _employees: string[] = [];
   constructor(n: string) {
     this.name = n;
   }
+
   describe(this: Department) {
     console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this._employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this._employees.length);
+    console.log(this._employees);
   }
 }
 
 let accounting = new Department("Accounting");
 console.log(accounting);
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
 accounting.describe();
+
 //nesne kopyalama işlemi
-const accountingCopy = { name: "dummy", describe: accounting.describe };
-accountingCopy.describe();
+//const accountingCopy = { name: "dummy", describe: accounting.describe };
+//accountingCopy.describe();
