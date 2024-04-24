38839faf7f6eaa2692c74ca972d4c8da428ef0d5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-24 13:49:06 +0300

    practised arrow functions as usual

--- Lesson13to21-NextGen-JS/app.ts
+++ Lesson13to21-NextGen-JS/app.ts
@@ -1,5 +1,17 @@
-//let and const ES6
+//let and const ES6 (var global scope ve fonksiyon scope haricini bilmez)
 const userName = "B";
 let age = 24;
 
 
+
+//Arrow Functions (this kullanılamaz)
+let fon = (a: number) => {
+  console.log("fon" + a);
+};
+fon(1);
+const printOutput: (a: number | string) => void = (output) => console.log(output);
+
+const button = document.querySelector("button");
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
+}
