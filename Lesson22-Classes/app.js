00b971e2036d0ebcd46d942b503b7b6d7c6a5134
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-24 20:27:32 +0300

    practised class structure

--- Lesson22-Classes/app.js
+++ Lesson22-Classes/app.js
@@ -1,8 +1,14 @@
 "use strict";
 class Department {
-  constructor(n) {
-    this.name = n;
-  }
+    constructor(n) {
+        this.name = n;
+    }
+    describe() {
+        console.log("Department: " + this.name);
+    }
 }
 let accounting = new Department("Accounting");
 console.log(accounting);
+accounting.describe();
+const accountingCopy = { name: "dummy", describe: accounting.describe };
+accountingCopy.describe();
--- Lesson22-Classes/app.ts
+++ Lesson22-Classes/app.ts
@@ -3,7 +3,14 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
 
 let accounting = new Department("Accounting");
 console.log(accounting);
+accounting.describe();
+//nesne kopyalama işlemi
+const accountingCopy = { name: "dummy", describe: accounting.describe };
+accountingCopy.describe();
