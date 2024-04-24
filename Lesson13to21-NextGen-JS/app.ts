f74daeb6c921d6c3c50d1613b811eb58e5d0907c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-24 13:53:20 +0300

    practised default argument order on functions

--- Lesson13to21-NextGen-JS/app.ts
+++ Lesson13to21-NextGen-JS/app.ts
@@ -2,16 +2,21 @@
 const userName = "B";
 let age = 24;
 
-
-
 //Arrow Functions (this kullanılamaz)
 let fon = (a: number) => {
   console.log("fon" + a);
 };
 fon(1);
-const printOutput: (a: number | string) => void = (output) => console.log(output);
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
 
 const button = document.querySelector("button");
 if (button) {
   button.addEventListener("click", (event) => console.log(event));
 }
+
+//Default Function Parameters (default parameters en son argümanlar olmalı yoksa hata verir)
+const add = (a: number, b: number = 1) => {
+  return a + b;
+};
+add(5);
