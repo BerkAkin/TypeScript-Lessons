71dc36fc9faef65a3f202a6cd28a49044e8bc6e4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-30 11:50:49 +0300

    practised singleton and private constructor

--- Lesson25-AbstractClasses-PrivateConstructors-Singletons/app.js
+++ Lesson25-AbstractClasses-PrivateConstructors-Singletons/app.js
@@ -32,6 +32,30 @@ var ITDepartment = /** @class */ (function (_super) {
     };
     return ITDepartment;
 }(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, name) {
+        var _this = _super.call(this, id, name) || this;
+        _this._date = "2024";
+        return _this;
+    }
+    AccountingDepartment.prototype.describe = function () {
+        console.log("Accounting Department - ID: " + this._id);
+    };
+    AccountingDepartment.getInstance = function () {
+        if (AccountingDepartment.instance) {
+            return AccountingDepartment.instance;
+        }
+        else {
+            this.instance = new AccountingDepartment("d1", "Accounting");
+            return this.instance;
+        }
+    };
+    return AccountingDepartment;
+}(Department));
+var accounting = AccountingDepartment.getInstance();
+var accounting2 = AccountingDepartment.getInstance();
+console.log("1:", accounting, "2", accounting2);
 var abs = new ITDepartment("d1", "IT");
 console.log(abs);
 abs.describe();
--- Lesson25-AbstractClasses-PrivateConstructors-Singletons/app.ts
+++ Lesson25-AbstractClasses-PrivateConstructors-Singletons/app.ts
@@ -19,6 +19,46 @@ class ITDepartment extends Department {
   }
 }
 
+class AccountingDepartment extends Department {
+  private static instance: AccountingDepartment;
+  public _date: string = "2024";
+  private constructor(id: string, name: string) {
+    super(id, name);
+  }
+  describe() {
+    console.log(`Accounting Department - ID: ` + this._id);
+  }
+  static getInstance() {
+    if (AccountingDepartment.instance) {
+      return AccountingDepartment.instance;
+    } else {
+      this.instance = new AccountingDepartment("d1", "Accounting");
+      return this.instance;
+    }
+  }
+}
+
+//SINGLETON CLASS
+class SingletonClass {
+  private _id: string;
+  private static instance: SingletonClass;
+
+  private constructor(id: string) {
+    this._id = id;
+  }
+  static createInstance(id: string) {
+    if (SingletonClass.instance) {
+      return SingletonClass.instance;
+    } else {
+      this.instance = new SingletonClass(id);
+      return this.instance;
+    }
+  }
+}
+
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+console.log("1:", accounting, "2", accounting2);
 const abs = new ITDepartment("d1", "IT");
 console.log(abs);
 abs.describe();
