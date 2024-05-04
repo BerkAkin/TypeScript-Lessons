8a8e854db9d73f7b9cc37938982d3c71a275afc0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-04 13:40:48 +0300

    practised discriminated unions

--- Lesson31-Advanced-Types/app.ts
+++ Lesson31-Advanced-Types/app.ts
@@ -26,3 +26,35 @@ const e1: ElevatedEmployee = {
 type Numeric = number | boolean;
 type Combinable = string | number;
 type Universal = Numeric & Combinable;
+
+//typeof, instanceof ve 'priv' in emp şeklindeki kontroller type guarding olarak bilinir.
+
+//DISCRIMINATED UNIONS
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("Moving with speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
+moveAnimal({ type: "horse", runningSpeed: 80 });
