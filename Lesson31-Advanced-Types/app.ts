6bffcf49ab7cdb31333e6d99d194092f2ff3b7e4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-08 13:17:09 +0300

    practsied function overloads

--- Lesson31-Advanced-Types/app.ts
+++ Lesson31-Advanced-Types/app.ts
@@ -83,4 +83,16 @@ const error: errorContainer = {
 };
 
 //FUNCTION OVERLOADS
- 
+function add(a: number): number;
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: any, b?: any) {
+  if (typeof a === "string" || typeof b == "string") {
+    return a.toString() + b.toString();
+  } else {
+    return a + b;
+  }
+}
+
+const total = add(1, 2);
+const total2 = add("1", "2");
