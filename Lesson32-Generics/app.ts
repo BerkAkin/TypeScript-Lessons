77109d302881ace1d56eb11488809d68dd40809b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-08 22:28:22 +0300

    practised generic definition in a great example

--- Lesson32-Generics/app.ts
+++ Lesson32-Generics/app.ts
@@ -8,9 +8,44 @@ const promise: Promise<number> = new Promise((resolve, reject) => {
   }, 2000);
 });
 
-promise.then((data) => {
+promise.then((data) => {""
   data.toFixed();
 });
+*/
 
 //CREATING OWN GENERIC TYPES
 
+function merge<T, U>(a: T, b: U) {
+  return { ...a, ...b };
+}
+const merged = merge({ name: "Berk" }, { age: 24 });
+console.log(merged.age);
+
+//generic types with multiple types
+const merged2 = merge<{ name: string; hobbies: string[] }, { age: number }>({ name: "Berk", hobbies: ["Sports"] }, { age: 24 });
+const merged3 = merge(1, 2);
+const merged4 = merge("1", "2");
+
+//WORKING WITH CONSTRAINTS
+
+function merger<T extends object, U extends object>(a: T, b: U) {
+  return Object.assign(a, b);
+}
+
+//SOME OTHER GENERIC FUNCTIONS
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe(["Cooking", "Sports"]));
