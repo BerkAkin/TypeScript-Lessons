06699f92e70082a431334fbd00fa8a3a6dd1a02c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-09 19:14:19 +0300

    practised decorator factories which can accept a parameter

--- Lesson34-Decorators/app.ts
+++ Lesson34-Decorators/app.ts
@@ -14,3 +14,18 @@ class Person {
 
 const pers = new Person();
 console.log(pers);
+
+//DECORATOR FACTORIES
+function Factory(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+@Factory("Logging-Animal")
+class Animal {
+  constructor() {
+    console.log("Animal Created");
+  }
+}
