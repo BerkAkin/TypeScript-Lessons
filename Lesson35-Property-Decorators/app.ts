bcf19c73108147dda8873293affe32ca5b26aedd
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-10 19:50:51 +0300

    did some autobinder example

--- Lesson35-Property-Decorators/app.ts
+++ Lesson35-Property-Decorators/app.ts
@@ -36,6 +36,7 @@ function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
 function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
   console.log("Method Decorator");
   console.log(target, name, descriptor);
+  return;
 }
 
 //PARAMETER DECORATORS
@@ -43,3 +44,29 @@ function Log4(target: any, name: string, position: number) {
   console.log("Parameter Decorator");
   console.log(target, name, position);
 }
+
+//AUTOBINDER EXAMPLE
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjustedDescriptor;
+}
+
+class Printer {
+  message = "This works";
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
