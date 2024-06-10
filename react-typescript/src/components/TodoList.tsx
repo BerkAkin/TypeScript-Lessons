75ac9313db38ff4e122bd128ab58212092302957
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-10 10:00:49 +0300

    FEAT: created TodoList component and defined an interface for getting a proper input.

---
+++ react-typescript/src/components/TodoList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface TodoListProps {
+  items: { id: string; text: string }[];
+}
+
+function TodoList({ items }: TodoListProps) {
+  return (
+    <>
+      <ul>
+        {items.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default TodoList;
